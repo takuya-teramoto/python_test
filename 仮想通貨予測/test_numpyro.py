b3c52081e03911488a878fb0cf816e4821741eb9
takuya-teramoto <t.teramon23@gmail.com>
2020-10-11 16:26:47 +0900

    Update test_numpyro.py
    入力値xが欠損値を含む場合の計算処理

--- 仮想通貨予測/test_numpyro.py
+++ 仮想通貨予測/test_numpyro.py
@@ -10,7 +10,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from jax import random
+from jax import ops,random
 import jax.numpy as jnp
 
 import numpyro
@@ -19,33 +19,61 @@ from numpyro.infer import MCMC, NUTS, Predictive
 from numpyro.infer.reparam import LocScaleReparam
 
 def model(x_obs, y_obs=None):
+    isnan = np.isnan(x_obs)
+    x_nanidx = np.nonzero(isnan)[0]
     x_mu = numpyro.sample("x_mu", dist.Uniform(-100, 100))
     x_sigma = numpyro.sample('x_sigma', dist.Uniform(0, 10))
-    x = numpyro.sample('x', dist.Normal(x_mu, x_sigma), obs=x_obs)
+    x_impute = numpyro.sample("x_impute", dist.Normal(x_mu, x_sigma).mask(False))
+    x_obs = ops.index_update(x_obs, x_nanidx, x_impute)
+    numpyro.sample('x', dist.Normal(x_mu, x_sigma), obs=x_obs)
+    
     a_mu = numpyro.sample("a_mu", dist.Uniform(-100, 100))
     a_sigma = numpyro.sample('a_sigma', dist.Uniform(0, 10))
     a = numpyro.sample('a', dist.Normal(a_mu, a_sigma))
+    
     b_mu = numpyro.sample("b_mu", dist.Uniform(-100, 100))
     b_sigma = numpyro.sample('b_sigma', dist.Uniform(0, 10))
     b = numpyro.sample('b', dist.Normal(b_mu, b_sigma))
-    y_mu = a * x + b
+    
+    y_mu = a * x_obs + b
     y_sigma = numpyro.sample('y_sigma', dist.Uniform(0, 10))
-    y = numpyro.sample('y_mu', dist.Normal(y_mu, y_sigma), obs=y_obs)
+    numpyro.sample('y', dist.Normal(y_mu, y_sigma), obs=y_obs)
 
+# memo
+# type(x_obs)
+# <class 'numpy.ndarray'>
+# type(x_nanidx)
+# <class 'numpy.ndarray'>
+# type(x_impute)
+# <class 'jax.interpreters.xla.DeviceArray'>
+# type(x_obs)
+# <class 'jax.interpreters.xla.DeviceArray'> 27行目でx_obsのtypeが変わる
 
 # 初期設定
 num_samples = 2000
 num_warmup = 200
-num_chains = 2
+num_chains = 1
+num_x_missings = 1500
+num_y_missings = 1500
 rng_key = random.PRNGKey(0)
 numpyro.set_platform('cpu')
 numpyro.set_host_device_count(num_chains)
 
 # 適当な関数生成
+np.random.seed(0)
 n = 1000
-x = np.random.randn(n)
-y = 3*x + 2*np.random.randn(n)
-plt.scatter(x, y)
+origin_x = np.random.randn(n)
+origin_y = 3*origin_x + 2*np.random.randn(n)
+plt.scatter(origin_x, origin_y)
+
+# 欠損値を発生させる
+x_missing_list = np.random.choice(list(range(n)), num_x_missings)
+y_missing_list = np.random.choice(list(range(n)), num_y_missings)
+
+x = origin_x.copy()
+y = origin_y.copy()
+x[x_missing_list] = np.nan
+# y[y_missing_list] = np.nan
 
 # numpyroでサンプリング
 kernel = NUTS(model)
@@ -54,4 +82,7 @@ mcmc.run(rng_key, x, y)
 mcmc.print_summary()
 samples = mcmc.get_samples()
 pred = Predictive(model, samples)(rng_key, x)
+
+plt.scatter(x, y)
+plt.scatter(pred['x'].mean(axis=0), pred['y'].mean(axis=0))
 
